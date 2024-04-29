42419059d54c8b995205e72121e3ab67d5f59a44
Caroline Caleguer <102368961+carolcal@users.noreply.github.com>
2024-04-29 12:32:54 -0300

    Update firebase.js

--- src/databases/firebase.js
+++ src/databases/firebase.js
@@ -7,14 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    authDomain: "trade-manager-91a41.firebaseapp.com",
-    projectId: "trade-manager-91a41",
-    storageBucket: "trade-manager-91a41.appspot.com",
-    messagingSenderId: "906564430141",
-    appId: "1:906564430141:web:1ca4ee29c829b46f623d54",
-    measurementId: "G-L50R46Z7YM"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
